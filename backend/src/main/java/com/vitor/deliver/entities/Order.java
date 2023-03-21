8fa9cbeb8821769dd03b8708ece27049ff531966
vitorriboli <vitorriboli1@gmail.com>
2023-03-21 07:25:59 -0300

    Entities

---
+++ backend/src/main/java/com/vitor/deliver/entities/Order.java
@@ -0,0 +1,116 @@
+package com.vitor.deliver.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import com.vitor.deliver.entities.enums.OrderStatus;
+
+//Serializable é uma boa medida para que os itens da classe possam ser transformados
+//em uma sequência de bites
+
+public class Order implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String address;
+	private Double latitude;
+	private Double longitude;
+	private Instant moment;
+	private OrderStatus status;
+	
+	
+	private Set<Product> products = new HashSet<>();
+	//Implementar somente a relação unidirecional
+	//O pedido conhece a coleção de produtos
+	//Product será simplificado a implementação bidirecional
+
+	public Order() {
+		super();
+	}
+
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+}
---
+++ backend/src/main/java/com/vitor/deliver/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.vitor.deliver.entities.enums;
+
+public enum OrderStatus {
+
+	PENDING, DELIVERED
+	
+	
+}
